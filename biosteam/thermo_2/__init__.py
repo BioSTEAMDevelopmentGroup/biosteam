9a9dc3857c05e4b4743e4b1992339af82b75d969
yoelcortes <yoelcortes@gmail.com>
2019-10-05 09:40:47 -0500

    update

--- biosteam/thermo_2/__init__.py
+++
@@ -1,4 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import thermo_model
-from . import heat_capacity
--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     name='biosteam',
     packages=['biosteam'],
     license='MIT',
-    version='0.9.0.15',
+    version='0.9.0.16',
     description='The Biorefinery Simulation and Techno-Economic Analysis Modules',
     long_description=open('README.rst').read(),
     #ext_modules=cythonize('biosteam/equilibrium/unifac.pyx'),
