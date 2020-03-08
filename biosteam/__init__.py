118a2391dfe762f1a761683bebc87827c3d7d9ae
yoelcortes <yoelcortes@gmail.com>
2020-03-07 21:34:41 -0600

    update

--- biosteam/__init__.py
+++ biosteam/__init__.py
@@ -5,9 +5,12 @@ Created on Sat Oct 28 17:28:09 2017
 
 @author: Yoel Rene Cortes-Pena
 """
+from thermosteam import *
+from thermosteam import __all__
+
 __all__ = ['Unit', 'PowerUtility', 'HeatUtility', 'UtilityAgent',
            'System', 'TEA', 'CombinedTEA', 
-           'find', 'Flowsheet', 'CE']
+           'find', 'Flowsheet', 'CE', *__all__]
 
 from lazypkg import LazyPkg
 #: Chemical engineering plant cost index (defaults to 567.5 at 2017)
