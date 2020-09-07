031cc7a62dc828a9efd233a2bd9d2868e0fe2f52
Yoel <yoelcortes@gmail.com>
2020-09-07 14:00:04 -0500

    use distutils

--- setup.py
+++ setup.py
@@ -5,9 +5,7 @@
 # This module is under the UIUC open-source license. See 
 # github.com/BioSTEAMDevelopmentGroup/biosteam/blob/master/LICENSE.txt
 # for license details.
-from setuptools import setup
-#from Cython.Build import cythonize
-#import numpy
+from distutils.core import setup
 
 setup(
     name='biosteam',
@@ -20,7 +18,6 @@ setup(
     install_requires=['IPython>=7.9.0', 'biorefineries>=2.15.11',
                       'thermosteam>=0.20.18', 'graphviz>=0.8.3',
                       'chaospy>=3.0.11', 'pipeml>=0.1'],
-    python_requires=">=3.6",
     package_data=
         {'biosteam': ['report/*',
                       'digraph/*',
@@ -42,13 +39,27 @@ setup(
     author_email='yoelcortes@gmail.com',
     url='https://github.com/BioSTEAMDevelopmentGroup/biosteam',
     download_url='https://github.com/BioSTEAMDevelopmentGroup/biosteam.git',
-    classifiers=['Development Status :: 3 - Alpha',
+    classifiers=['License :: OSI Approved :: University of Illinois/NCSA Open Source License',
+                 'Development Status :: 3 - Alpha',
                  'Environment :: Console',
-                 'License :: OSI Approved :: University of Illinois/NCSA Open Source License',
-                 'Programming Language :: Python :: 3.6',
-				 'Programming Language :: Python :: 3.7',
                  'Topic :: Scientific/Engineering',
                  'Topic :: Scientific/Engineering :: Chemistry',
-                 'Topic :: Scientific/Engineering :: Mathematics'],
-    keywords='chemical process simmulation bioprocess engineering mass energy balance material properties phase equilibrium CABBI biorefinery biofuel bioproducts',
+                 'Topic :: Scientific/Engineering :: Mathematics',
+                 'Intended Audience :: Developers',
+                 'Intended Audience :: Education',
+                 'Intended Audience :: Manufacturing',
+                 'Intended Audience :: Science/Research',
+                 'Natural Language :: English',
+                 'Operating System :: MacOS',
+                 'Operating System :: Microsoft :: Windows',
+                 'Operating System :: POSIX',
+                 'Operating System :: POSIX :: BSD',
+                 'Operating System :: POSIX :: Linux',
+                 'Operating System :: Unix',
+                 'Programming Language :: Python :: 3.6',
+                 'Programming Language :: Python :: 3.7',
+                 'Programming Language :: Python :: 3.8',
+                 'Programming Language :: Python :: Implementation :: CPython',
+                 'Topic :: Education'],
+    keywords=['chemical process simmulation', 'bioprocess engineering', 'mass and energy balance', 'material properties', 'phase equilibrium', 'CABBI', 'biorefinery', 'biofuel', 'bioproducts'],
 )
