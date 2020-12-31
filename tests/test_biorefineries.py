fb3ecb48738a69c96bf20e07e9a1d399557d6528
Yoel <yoelcortes@gmail.com>
2020-12-31 12:41:56 -0600

    fix biorefinery tests to ignore missing biorefineries

--- tests/test_biorefineries.py
+++ tests/test_biorefineries.py
@@ -23,9 +23,9 @@ try: test_LAOs = ignore_import_error(test_LAOs)
 except NameError: pass
 try: test_lactic = ignore_import_error(test_lactic)
 except NameError: pass
-try: test_ethanol_adipic = ignore_import_error(test_annimal_bedding)
+try: test_ethanol_adipic = ignore_import_error(test_ethanol_adipic)
 except NameError: pass
-try: test_annimal_bedding = ignore_import_error(test_annimal_bedding)
+try: test_animal_bedding = ignore_import_error(test_animal_bedding)
 except NameError: pass
 try: test_wheatstraw = ignore_import_error(test_wheatstraw)
 except NameError: pass
