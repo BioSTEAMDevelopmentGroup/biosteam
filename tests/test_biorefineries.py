d6011b118f8494ed49f82bc7c014b6d713c18c75
Yoel <yoelcortes@gmail.com>
2021-01-02 13:37:02 -0600

    ingore missing biorefineries in tests

--- tests/test_biorefineries.py
+++ tests/test_biorefineries.py
@@ -23,6 +23,10 @@ try: test_LAOs = ignore_import_error(test_LAOs)
 except NameError: pass
 try: test_lactic = ignore_import_error(test_lactic)
 except NameError: pass
+try: test_HP_cellulosic = ignore_import_error(test_HP_cellulosic)
+except NameError: pass
+try: test_HP_sugarcane = ignore_import_error(test_HP_sugarcane)
+except NameError: pass
 try: test_ethanol_adipic = ignore_import_error(test_ethanol_adipic)
 except NameError: pass
 try: test_animal_bedding = ignore_import_error(test_animal_bedding)
