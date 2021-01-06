6e705aea2fa7789045f8e5b730e2899abaf48d24
Yoel <yoelcortes@gmail.com>
2021-01-06 15:28:51 -0600

    update documentation and version for equipment lifetime feature

--- biosteam/__init__.py
+++ biosteam/__init__.py
@@ -8,7 +8,7 @@
 """
 """
 
-__version__ = '2.22.6'
+__version__ = '2.22.7'
 
 #: Chemical engineering plant cost index (defaults to 567.5 at 2017).
 CE = 567.5 
--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     name='biosteam',
     packages=['biosteam'],
     license='MIT',
-    version='2.22.6',
+    version='2.22.7',
     description='The Biorefinery Simulation and Techno-Economic Analysis Modules',
     long_description=open('README.rst').read(),
     author='Yoel Cortes-Pena',
