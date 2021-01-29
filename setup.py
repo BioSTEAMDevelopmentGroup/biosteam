547dac5a14653bf135c317d611fa89071612e11b
Yoel <yoelcortes@gmail.com>
2021-01-28 23:08:24 -0600

    bump up version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     name='biosteam',
     packages=['biosteam'],
     license='MIT',
-    version='2.23.4',
+    version='2.23.5',
     description='The Biorefinery Simulation and Techno-Economic Analysis Modules',
     long_description=open('README.rst').read(),
     author='Yoel Cortes-Pena',
