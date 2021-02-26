d6ac3b2777659e456448a274d7058d82890ef4e2
Yoel <yoelcortes@gmail.com>
2021-02-26 10:05:25 -0600

    add LAOs to tests

--- tests/test_biorefineries.py
+++ tests/test_biorefineries.py
@@ -10,16 +10,15 @@
 from biorefineries.tests.test_biorefineries import (
     test_sugarcane,
     test_lipidcane,
-    test_cornstover
+    test_cornstover,
+    test_LAOs
 )
-
 
 # To run these following tests, you'll need to get the github version.
 # If you have the PyPI installation, these tests will not run.
 # This is OK, the loss in coverage is very small.
 
 # from biorefineries.tests import (
-#     test_LAOs,
 #     test_ethanol_adipic,
 #     test_lactic
 # )    
