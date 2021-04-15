3deffa44fa0ad5d310f8b39b49ef660d5a2c5250
Yoel <yoelcortes@gmail.com>
2021-04-15 00:26:02 -0500

    bump up version

--- biosteam/__init__.py
+++ biosteam/__init__.py
@@ -7,7 +7,7 @@
 # for license details.
 """
 """
-__version__ = '2.27.6'
+__version__ = '2.27.7'
 
 #: Chemical engineering plant cost index (defaults to 567.5 at 2017).
 CE = 567.5 
--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     name='biosteam',
     packages=['biosteam'],
     license='MIT',
-    version='2.27.6',
+    version='2.27.7',
     description='The Biorefinery Simulation and Techno-Economic Analysis Modules',
     long_description=open('README.rst').read(),
     author='Yoel Cortes-Pena',
