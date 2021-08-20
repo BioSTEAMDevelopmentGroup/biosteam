bdb0e3fac42c4fd9e04683a4d92edb2ab69cb480
Yoel <yoelcortes@gmail.com>
2021-08-20 18:58:29 -0500

    remove source suffix to prevent nbsphinx 0.8.7 from messing up parsing

--- docs/conf.py
+++ docs/conf.py
@@ -76,7 +76,7 @@ templates_path = ['_templates']
 # You can specify multiple suffix as a list of string:
 #
 # source_suffix = ['.rst', '.md']
-source_suffix = ['.txt', '.ipynb', '.rst']
+source_suffix = ['.txt', '.rst']
 
 # The master toctree document.
 master_doc = 'index'
