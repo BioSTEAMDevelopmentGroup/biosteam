c743344a98228d5d726794adfe0ad7aaa0fc2a82
Yoel Rene Cortes-Pena <yoelcortes@gmail.com>
2021-10-14 11:27:48 +0700

    add back heat utilities as optional to allow for different assumptions

--- biosteam/units/_molecular_sieve.py
+++ biosteam/units/_molecular_sieve.py
@@ -35,7 +35,7 @@ class MolecularSieve(Splitter):
     >>> from biosteam import Stream, settings
     >>> from biosteam.units import MolecularSieve
     >>> settings.set_thermo(['Water', 'Ethanol'], cache=True)
-    >>> feed = Stream('feed', flow=(75.7, 286), phase='g')
+    >>> feed = Stream('feed', flow=(75.7, 286), T=351.39, phase='g')
     >>> bp = feed.bubble_point_at_T()
     >>> feed.T = bp.T
     >>> MS1 = MolecularSieve('MS1', ins=feed,
@@ -47,18 +47,18 @@ class MolecularSieve(Splitter):
     MolecularSieve: MS1
     ins...
     [0] feed
-        phase: 'g', T: 25 degC, P: 1 atm
+        phase: 'g', T: 78.24 degC, P: 1 atm
         composition: Water    0.209
                      Ethanol  0.791
                      -------  362 kmol/hr
     outs...
     [0] ethanol_rich
-        phase: 'g', T: 25 degC, P: 1 atm
+        phase: 'g', T: 78.24 degC, P: 1 atm
         composition: Water    0.0438
                      Ethanol  0.956
                      -------  277 kmol/hr
     [1] water_rich
-        phase: 'g', T: 25 degC, P: 1 atm
+        phase: 'g', T: 78.24 degC, P: 1 atm
         composition: Water    0.748
                      Ethanol  0.252
                      -------  85 kmol/hr
@@ -67,10 +67,16 @@ class MolecularSieve(Splitter):
     Molecular sieve                  Units       MS1
     Power               Rate            kW      14.2
                         Cost        USD/hr      1.11
+    Low pressure steam  Duty         kJ/hr  3.21e+06
+                        Flow       kmol/hr      82.7
+                        Cost        USD/hr      19.7
+    Cooling water       Duty         kJ/hr -1.18e+05
+                        Flow       kmol/hr      80.9
+                        Cost        USD/hr    0.0395
     Design              Flow rate    kg/hr  2.13e+03
     Purchase cost       Column         USD  6.85e+05
     Total purchase cost                USD  6.85e+05
-    Utility cost                    USD/hr      1.11
+    Utility cost                    USD/hr      20.8
     
     References
     ----------
@@ -83,21 +89,23 @@ class MolecularSieve(Splitter):
         Washington and Atlanta, Georgia)
     
     """
-    _N_heat_utilities = 1
+    _N_heat_utilities = 2
     _units = {'Flow rate': 'kg/hr'}
     def __init__(self, ID='', ins=None, outs=(), *, order=None, split,
-                 P=101325, ):
+                 P=101325, approx_duty=True):
         Splitter.__init__(self, ID, ins, outs, order=order, split=split)
         self.P = 101325
+        self.approx_duty = approx_duty
         
     def _run(self):
         Splitter._run(self)
         for i in self.outs: i.P = self.P
 
     def _design(self):
-        self.design_results['Flow rate'] = self._outs[1].F_mass
-        # T = self.ins[0].T
-        # hu1, hu2 = self.heat_utilities
-        # hu1(1429.65*flow, T)
-        # hu2(-55.51*flow, T)
+        self.design_results['Flow rate'] = flow = self._outs[1].F_mass
+        if self.approx_duty:
+            T = self.outs[0].T
+            hu1, hu2 = self.heat_utilities
+            hu1(1429.65*flow, T)
+            hu2(-55.51*flow, T)
 
