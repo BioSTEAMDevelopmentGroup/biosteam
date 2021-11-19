0ced47edcaced05928700faf1a6f67d9f0b459cc
Yoel <yoelcortes@gmail.com>
2021-11-19 07:58:22 -0600

    2.31.18 release

--- biosteam/__init__.py
+++ biosteam/__init__.py
@@ -7,7 +7,7 @@
 # for license details.
 """
 """
-__version__ = '2.31.17'
+__version__ = '2.31.18'
 
 #: dict[str, float] Price of stream utilities (in USD/kg) which are defined as 
 #: inlets and outlets to unit operations.
