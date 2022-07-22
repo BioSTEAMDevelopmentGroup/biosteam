0237ea20c9e38dd822f93e816d035b646f618a48
Yoel <yoelcortes@gmail.com>
2022-07-21 19:20:13 -0500

    more utilities for easier biorefinery development

--- biosteam/utils/stream_filters.py
+++ biosteam/utils/stream_filters.py
@@ -9,18 +9,25 @@
 """
 import biosteam as bst
 
-__all__ = ('streams_from_units',
-           'process_streams',
-           'streams_from_path', 
-           'feeds',
-           'products',
-           'inlets',
-           'outlets',
-           'filter_out_missing_streams',
-           'sort_feeds_big_to_small',
-           'feeds_from_units',
-           'products_from_units',
-           'get_inlet_origin')
+__all__ = (
+    'streams_from_units',
+    'process_streams',
+    'streams_from_path', 
+    'feeds',
+    'products',
+    'inlets',
+    'outlets',
+    'filter_out_missing_streams',
+    'sort_feeds_big_to_small',
+    'feeds_from_units',
+    'products_from_units',
+    'get_inlet_origin',
+    'get_streams_from_context_level',
+    'get_fresh_process_water_streams',
+    'get_unaccounted_waste_streams',
+    'get_unaccounted_combustible_waste_streams',
+    'get_streams_from_context_level',
+)
 
 feed_priorities = {}
 
@@ -107,3 +114,151 @@ def set_feed_priority(stream, value):
     
 bst.Stream.get_feed_priority = get_feed_priority
 bst.Stream.set_feed_priority = set_feed_priority
+
+def get_streams_from_context_level(level=None):
+    """
+    Return streams created in given context level.
+    
+    Parameters
+    ----------
+    level : int, optional
+        If given, only filter through streams created in the given context 
+        level. For example, use:
+        * 0: to account for all streams created in the current context level.
+        * 1: to account for streams created in the next outer context level.
+    
+    Examples
+    --------
+    Get all waste streams created (as registerd in the flowsheet):
+        
+    >>> import biosteam as bst
+    >>> bst.main_flowsheet.clear()
+    >>> bst.settings.set_thermo(['Water', 'Ethanol'], cache=True)
+    >>> waste = bst.Stream('waste')
+    >>> mixer = bst.Mixer('mixer', outs=waste)
+    >>> streams = bst.get_streams_from_context_level()
+    >>> assert len(streams) == 1 and streams[0] == waste
+    
+    Get waste streams created within a context:
+        
+    >>> with bst.System('sys') as sys:
+    ...     new_waste = bst.Stream('new_waste')
+    ...     tank = bst.MixTank('tank', outs=new_waste)
+    ...     streams = bst.get_streams_from_context_level(level=0)
+    ...     assert len(streams) == 1 and streams[0] == new_waste
+    
+    """
+    if level is None:
+        streams = list(bst.main_flowsheet.stream)
+    else:
+        context_levels = bst.main_flowsheet.unit.context_levels
+        N_levels = len(context_levels)
+        if level >= N_levels:
+            streams = bst.main_flowsheet.stream
+        else:
+            index = N_levels - level - 1
+            units = context_levels[index]
+            streams = [i for i in streams_from_units(units) if i]
+    return streams
+
+def get_unaccounted_combustible_waste_streams(streams=None):
+    """
+    Return all product streams that do not have a price and have a lower 
+    heating value (LHV) greater than 1 kJ/g. These are assumed to be waste 
+    streams that can potentially be used to generate heat through combustion 
+    and have not been accounted for.
+    
+    Parameters
+    ----------
+    streams : Iterable[Stream], optional
+        Stream to filter through.
+    
+    Examples
+    --------
+    Get all waste streams created (as registerd in the flowsheet):
+        
+    >>> import biosteam as bst
+    >>> bst.main_flowsheet.clear()
+    >>> bst.settings.set_thermo(['Water', 'Ethanol'], cache=True)
+    >>> waste = bst.Stream('waste', Ethanol=1)
+    >>> mixer = bst.Mixer('mixer', outs=waste)
+    >>> streams = bst.get_unaccounted_waste_streams()
+    >>> assert len(streams) == 1 and streams[0] == waste
+    
+    """
+    if streams is None: streams = bst.main_flowsheet.stream
+    isa = isinstance
+    return [
+        i for i in streams if 
+        not i.price and i.isproduct()
+        and not isa(i.source, bst.Facility) 
+        and i.LHV / i.F_mass > 1000. 
+    ]
+
+def get_unaccounted_waste_streams(streams=None, phase=None):
+    """
+    Return all product streams that do not have a price at the given phase.
+    These are assumed to be waste streams that have not been accounted for 
+    (i.e., is not sold and needs to be treated in some way).
+    
+    Parameters
+    ----------
+    streams : Iterable[Stream], optional
+        Stream to filter through.
+    phase : str, optional
+        Phase of streams. Defaults to 'l' for liquid.
+    
+    Examples
+    --------
+    Get all waste streams created (as registerd in the flowsheet):
+        
+    >>> import biosteam as bst
+    >>> bst.main_flowsheet.clear()
+    >>> bst.settings.set_thermo(['Water', 'Ethanol'], cache=True)
+    >>> waste = bst.Stream('waste')
+    >>> mixer = bst.Mixer('mixer', outs=waste)
+    >>> streams = bst.get_unaccounted_waste_streams()
+    >>> assert len(streams) == 1 and streams[0] == waste
+    
+    """
+    if streams is None: streams = bst.main_flowsheet.stream
+    if phase is None: phase = 'l'
+    isa = isinstance
+    return [
+        i for i in streams if 
+        not i.price and i.isproduct()
+        and i.phase==phase
+        and not isa(i.source, bst.Facility)
+    ]
+
+def get_fresh_process_water_streams(streams=None):
+    """
+    Return all feed water streams without a price.
+    
+    Parameters
+    ----------
+    streams : Iterable[Stream], optional
+        Stream to filter through.
+        
+    Examples
+    --------
+    Get all fresh process water streams created (as registerd in the flowsheet):
+        
+    >>> import biosteam as bst
+    >>> bst.main_flowsheet.clear()
+    >>> bst.settings.set_thermo(['Water', 'Ethanol'], cache=True)
+    >>> water = bst.Stream('water', Water=1)
+    >>> mixer = bst.Mixer('mixer', ins=water)
+    >>> streams = bst.get_fresh_process_water_streams()
+    >>> assert len(streams) == 1 and streams[0] == water
+    
+    """    
+    def only_water(stream):
+        chemicals = stream.available_chemicals
+        return len(chemicals) == 1 and chemicals[0].CAS == '7732-18-5'
+    
+    if streams is None: streams = bst.main_flowsheet.stream
+    return [
+        i for i in streams
+        if not i.price and i.isfeed() and i.phase=='l' and only_water(i)
+    ]
