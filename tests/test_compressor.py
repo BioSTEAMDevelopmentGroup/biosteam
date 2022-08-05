472c93ec5f375c2458832e3f1ada07cd3476c202
Benjamin W. Portner <benjamin.portner@bauhaus-luftfahrt.net>
2022-08-05 12:38:56 +0200

    fix test_compressor part 1

--- tests/test_compressor.py
+++ tests/test_compressor.py
@@ -15,8 +15,9 @@ from thermo import SRK, PR
 
 def test_isentropic_hydrogen_compressor():
     bst.settings.set_thermo(["H2"])
+    eta = 0.7
     feed = bst.Stream(H2=1, T=25 + 273.15, P=101325, phase='g')
-    K = bst.units.IsentropicCompressor(ins=feed, P=50e5, eta=0.7)
+    K = bst.units.IsentropicCompressor(ins=feed, P=50e5, eta=eta)
     K.simulate()
     # check outlet state
     out = K.outs[0]
@@ -25,11 +26,17 @@ def test_isentropic_hydrogen_compressor():
         b=[1.0, 0.0, 1151.3251608356125, 50e5],
     )
     # check compressor design
+    assert K.design_results["Type"] == "Recriprocating"
+    assert K.design_results["Driver"] == "Electric motor"
+    ideal_power = 4.919879203671207
+    eta_motor = K.baseline_cost_algorithms[K.design_results["Type"]].efficiencies["Electric motor"]
+    expected_power = ideal_power / eta / eta_motor
+    actual_power = K.power_utility.rate
     assert allclose(
-        a=list(K.design_results.values())[1:],
-        b=[7.02839886238921, 0, 1151.3251608356125, 24.465403697038127, 4.919879203671207, 0, 901.1332666056242],
+        a=[actual_power, K.design_results['Ideal power'],
+           K.design_results['Ideal duty'], K.design_results['Compressors in parallel']],
+        b=[expected_power, ideal_power, 0, 1],
     )
-    assert K.design_results["Type"] == "Blower"
     pass
 
 
@@ -38,7 +45,8 @@ def test_isentropic_two_phase_steam_compressor():
     # feed is steam-water-mixture
     feed = bst.MultiStream(T=372.75, P=1e5, l=[('H2O', 0.1)], g=[('H2O', 0.9)])
     # from compress until superheated steam (test case for vle parameter)
-    K = bst.units.IsentropicCompressor(ins=feed, P=100e5, eta=1.0, vle=True)
+    eta = 1
+    K = bst.units.IsentropicCompressor(ins=feed, P=100e5, eta=eta, vle=True)
     K.simulate()
     # check outlet state
     out = K.outs[0]
@@ -47,13 +55,17 @@ def test_isentropic_two_phase_steam_compressor():
         b=[1.0, 0.0, 797.7528062886108, 100e5],
     )
     # check compressor design
+    assert K.design_results["Type"] == "Recriprocating"
+    assert K.design_results["Driver"] == "Electric motor"
+    ideal_power = 5.41038996517047
+    eta_motor = K.baseline_cost_algorithms[K.design_results["Type"]].efficiencies["Electric motor"]
+    expected_power = ideal_power / eta / eta_motor
+    actual_power = K.power_utility.rate
     assert allclose(
-        a=list(K.design_results.values())[1:2]+list(K.design_results.values())[3:],
-        b=[5.410389965766295, 797.7528062886108, 27.89482592365777, 5.410389965201038, 0, 797.7528062360269],
+        a=[actual_power, K.design_results['Ideal power'],
+           K.design_results['Ideal duty'], K.design_results['Compressors in parallel']],
+        b=[expected_power, ideal_power, 0, 1],
     )
-    assert K.design_results["Type"] == "Blower"
-    tol = 1e-6
-    assert abs(K.design_results["Duty"]) < 1e-6 # testing this with allclose does not work for some reason
     pass
 
 
@@ -63,11 +75,12 @@ def test_isothermal_hydrogen_compressor():
     bst.settings.set_thermo(thermo)
     feed = bst.Stream(H2=1, T=298.15, P=20e5, phase='g')
 
-    expected_power = 2.1024573963797715
-    expected_duty = -7261.402248757514
+    ideal_power = 2.1024573963797715
+    ideal_duty = -7261.402248757514
 
     # eta = 1
-    K = bst.units.IsothermalCompressor(ins=feed, P=350e5, eta=1)
+    eta = 1
+    K = bst.units.IsothermalCompressor(ins=feed, P=350e5, eta=eta)
     K.simulate()
     # check outlet state
     out = K.outs[0]
@@ -76,19 +89,26 @@ def test_isothermal_hydrogen_compressor():
         b=[1.0, 0.0, feed.T, 350e5],
     )
     # check compressor design
+    assert K.design_results["Type"] == "Recriprocating"
+    assert K.design_results["Driver"] == "Electric motor"
+    eta_motor = K.baseline_cost_algorithms[K.design_results["Type"]].efficiencies["Electric motor"]
+    expected_power = ideal_power / eta / eta_motor
+    actual_power = K.power_utility.rate
     assert allclose(
-        a=list(K.design_results.values())[1:],
-        b=[expected_power, expected_duty, feed.T, 1.2394785148011942, expected_power, expected_duty, feed.T],
+        a=[actual_power, K.design_results['Ideal power'],
+           K.design_results['Ideal duty'], K.design_results['Compressors in parallel']],
+        b=[expected_power, ideal_power, ideal_duty, 1],
     )
-    assert K.design_results["Type"] == "Blower"
     # check heat utility
+    expected_duty = ideal_duty / eta
     assert allclose(
         a=[K.heat_utilities[0].unit_duty, K.heat_utilities[0].duty, K.heat_utilities[0].flow],
         b=[expected_duty, expected_duty, 7.525952491732307],
     )
 
     # repeat with eta=0.7
-    K = bst.units.IsothermalCompressor(ins=feed, P=350e5, eta=0.7)
+    eta = 0.7
+    K = bst.units.IsothermalCompressor(ins=feed, P=350e5, eta=eta)
     K.simulate()
     # check outlet state
     out = K.outs[0]
@@ -97,15 +117,21 @@ def test_isothermal_hydrogen_compressor():
         b=[1.0, 0.0, feed.T, 350e5],
     )
     # check compressor design
+    assert K.design_results["Type"] == "Recriprocating"
+    assert K.design_results["Driver"] == "Electric motor"
+    eta_motor = K.baseline_cost_algorithms[K.design_results["Type"]].efficiencies["Electric motor"]
+    expected_power = ideal_power / eta / eta_motor
+    actual_power = K.power_utility.rate
     assert allclose(
-        a=list(K.design_results.values())[1:],
-        b=[expected_power/0.7, expected_duty/0.7, feed.T, 1.2394785148011942, expected_power, expected_duty, feed.T],
+        a=[actual_power, K.design_results['Ideal power'],
+           K.design_results['Ideal duty'], K.design_results['Compressors in parallel']],
+        b=[expected_power, ideal_power, ideal_duty, 1],
     )
-    assert K.design_results["Type"] == "Blower"
     # check heat utility
+    expected_duty = ideal_duty / eta
     assert allclose(
         a=[K.heat_utilities[0].unit_duty, K.heat_utilities[0].duty, K.heat_utilities[0].flow],
-        b=[expected_duty/0.7, expected_duty/0.7, 10.751360702474724],
+        b=[expected_duty, expected_duty, 7.525952491732307],
     )
     pass
 
@@ -121,22 +147,22 @@ def test_polytropic_hydrogen_compressor():
     K = bst.units.PolytropicCompressor(ins=feed, P=P, eta=eta, method="schultz")
     K.simulate()
     out_poly = K.outs[0]
-    K_isen = bst.units.IsentropicCompressor(ins=feed, P=P, eta=1.0)
+    K_isen = bst.units.IsentropicCompressor(ins=feed, P=P, eta=eta)
     K_isen.simulate()
     out_isen = K_isen.outs[0]
+    assert K.heat_utilities == ()
     assert allclose(
-        a=[out_poly.T, out_poly.P, out_poly.H, K.design_results["Power"], K.design_results["Duty"]],
-        b=[out_isen.T, out_isen.P, out_isen.H, K_isen.design_results["Power"], 0],
-        # note: do not use K_isen duty due to rounding errors
+        a=[out_poly.T, out_poly.P, out_poly.H, K.power_utility.rate],
+        b=[out_isen.T, out_isen.P, out_isen.H, K_isen.power_utility.rate],
     )
     # eta=1, hundseid method
     K = bst.units.PolytropicCompressor(ins=feed, P=P, eta=eta, method="hundseid")
     K.simulate()
     out_poly = K.outs[0]
+    assert K.heat_utilities == ()
     assert allclose(
-        a=[out_poly.T, out_poly.P, out_poly.H, K.design_results["Power"], K.design_results["Duty"]],
-        b=[out_isen.T, out_isen.P, out_isen.H, K_isen.design_results["Power"], 0],
-        # note: do not use K_isen duty due to rounding errors
+        a=[out_poly.T, out_poly.P, out_poly.H, K.power_utility.rate],
+        b=[out_isen.T, out_isen.P, out_isen.H, K_isen.power_utility.rate],
     )
     # eta=0.7, hundseid method
     eta=0.7
@@ -150,11 +176,22 @@ def test_polytropic_hydrogen_compressor():
         b=[1.0, 0.0, 958.12, P],
     )
     # check compressor design
+    assert K.design_results["Type"] == "Recriprocating"
+    assert K.design_results["Driver"] == "Electric motor"
+    ideal_duty = 0
+    ideal_power = K_isen.power_utility.rate
+    eta_motor = K.baseline_cost_algorithms[K.design_results["Type"]].efficiencies["Electric motor"]
+    expected_power = ideal_power / eta / eta_motor
+    actual_power = K.power_utility.rate
+    assert allclose(
+        a=[actual_power, K.design_results['Ideal power'],
+           K.design_results['Ideal duty'], K.design_results['Compressors in parallel']],
+        b=[expected_power, ideal_power, ideal_duty, 1],
+    )
     assert allclose(
         a=list(K.design_results.values())[1:],
         b=[5.510063319708909, 0, 958.12, 1.2394785148011942],
     )
-    assert K.design_results["Type"] == "Blower"
 
 
 def test_multistage_hydrogen_compressor_simple():
@@ -263,17 +300,17 @@ def test_compressor_design():
         K.simulate()
     # test user overwrite
     feed.F_mol = 1e4
-    K = bst.units.IsothermalCompressor(ins=feed, P=350e5, type="Reciprocating")
+    K = bst.units.IsothermalCompressor(ins=feed, P=350e5, compressor_type="Reciprocating")
     K.simulate()
     assert K.design_results["Type"] == "Reciprocating"
     pass
 
 
 if __name__ == '__main__':
-    test_compressor_design()
+    # test_compressor_design()
     test_isentropic_hydrogen_compressor()
     test_isentropic_two_phase_steam_compressor()
-    test_isothermal_hydrogen_compressor()
+    # test_isothermal_hydrogen_compressor()
     test_polytropic_hydrogen_compressor()
     test_multistage_hydrogen_compressor_simple()
     test_multistage_hydrogen_compressor_advanced()
