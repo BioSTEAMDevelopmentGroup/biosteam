b9a9b4b0a563c903fdd8225fdb4f95571633aae1
lkudli2 <91288134+lkudli2@users.noreply.github.com>
2022-09-26 15:42:28 -0500

    enhancement to init

--- biosteam/units/_transesterification.py
+++ biosteam/units/_transesterification.py
@@ -51,18 +51,20 @@ class Transesterification(Unit):
                 ('Working volume fraction', 0.8, ''))
 
     def __init__(self, ID='', ins=None, outs=(), *,
-                 efficiency, excess_methanol, T, x_catalyst, transesterification=None):
+                 efficiency=None, excess_methanol, T, x_catalyst, transesterification=None,
+                 ):
         Unit.__init__(self, ID, ins, outs)
         if transesterification is None:
+            if efficiency is None: efficiency = 0.9
             #: [:class:`~thermosteam.ParallelReaction`] Transesterification and catalyst consumption reactions.
             self.transesterification = ParallelReaction([
                 Reaction('MAG + Methanol -> Biodiesel + Glycerol', reactant='MAG',  X=efficiency),
                 Reaction('DAG + 2Methanol -> 2Biodiesel + Glycerol', reactant='DAG',  X=efficiency),
                 Reaction('TAG + 3Methanol -> 3Biodiesel + Glycerol', reactant='TAG',  X=efficiency),
-                Reaction('NaOCH3 -> NaOH + Methanol', reactant='NaOCH3', X=1)
             ])
         else:
-           self.transesterification = transesterification           
+           self.transesterification = transesterification
+        self.catalyst_dissolution = Reaction('NaOCH3 -> NaOH + Methanol', reactant='NaOCH3', X=1)
         self.x_catalyst = x_catalyst #: [float] Catalyst molar fraction in methanol feed.
         self.excess_methanol = excess_methanol #: [float] Excess methanol fed in addition to the required stoichiometric amount.
         self.T = T #: [float] Operating temperature (K).
@@ -78,10 +80,10 @@ class Transesterification(Unit):
     @property
     def efficiency(self):
         """Transesterification efficiency."""
-        return self.transesterification.X[:-1].mean()
+        return self.transesterification.X.mean()
     @efficiency.setter
     def efficiency(self, efficiency):
-        self.transesterification.X[:-1] = efficiency
+        self.transesterification.X[:] = efficiency
         
     def _run(self):
         feed, methanol = self.ins
@@ -95,6 +97,7 @@ class Transesterification(Unit):
         methanol.imol['NaOCH3'] =  x_catalyst / x_methanol * methanol.imol['Methanol'] 
         product.mix_from([feed, methanol], energy_balance=False)
         self.transesterification(product)
+        self.catalyst_dissolution(product)
         product.T = self.T
         
     def _design(self):
