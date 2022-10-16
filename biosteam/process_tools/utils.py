0cc8f627e6d26cc0b4b18a6be9fbadb1a98de076
Yoel <yoelcortes@gmail.com>
2022-10-16 15:05:31 -0500

    update based function to use hxn_ok attribute

--- biosteam/process_tools/utils.py
+++ biosteam/process_tools/utils.py
@@ -228,7 +228,7 @@ def heat_exchanger_utilities_from_units(units):
     including the condensers and boilers of distillation columns and
     flash vessel heat exchangers."""
     heat_utilities = sum([i.heat_utilities for i in units], [])
-    return [i for i in heat_utilities if i.heat_exchanger]
+    return [i for i in heat_utilities if i.hxn_ok]
 
 def ID_number(ID):
     """
