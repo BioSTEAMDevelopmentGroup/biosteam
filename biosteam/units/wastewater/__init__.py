58b2de0bdf4ab7ad282d276eb1663fd685c310e3
Yalin Li <mailto.yalin.li@gmail.com>
2023-03-16 08:36:31 -0500

    add wrapper function for wastewater treatment system creation

--- biosteam/units/wastewater/__init__.py
+++ biosteam/units/wastewater/__init__.py
@@ -14,8 +14,59 @@ from ._wwt_pump import *
 from ._polishing_filter import *
 from ._membrane_bioreactor import *
 from ._sludge_handling import *
-from .conventional import *
-from .high_rate import *
+
+from .conventional import create_conventional_wastewater_treatment_system
+from .high_rate import create_high_rate_wastewater_treatment_system
+
+def create_wastewater_treatment_system(WWT='conventional', **WWT_kwargs):
+    '''
+    Create a wastewater treatment (WWT) system.
+    Two configurations are available: conventional and high-rate.
+    
+    The conventional configuration is based on the WWT process describe in Humbird et al.,
+    while the high-rate process is based on the one described in Li et al.
+    
+    Both configurations are designed to use biological processes to remove 
+    organics in the wastewater, and use reverse osmosis to remove the ions, 
+    after which the treated water is recycled as process water to be used in the biorefinery.
+    
+    The main differences between the configurations is that the conventional process
+    uses large anaerobic and aerobic basins for organic removal, 
+    which has a very low organic loading (grams COD/L/d).
+    While the high-rate process uses high-rate anaerobic technologies including
+    internal circulation and anaerobic membrane bioreactor with a much higher
+    organic loading (>10X of the conventional ones).
+    
+    With the much higher organic loading rate (smaller reactor and physical footprint)
+    and the use of two anaerobic unit operations 
+    (anaerobic process does not need aeration therefore saves electricity),
+    the high-rate process is expected to have lower CAPEX/OPEX with 
+    lower environmental impacts.
+    
+    Please see the specific functions below for more details on input parameters.
+    
+    Parameters
+    ----------
+    WWT : str
+        Either "conventional" for the conventional configuration,
+        or "high-rate" for the high-rate process.
+        All remaining kwargs will be passed to either 
+        `create_conventional_wastewater_treatment_system` or
+        `create_high_rate_wastewater_treatment_system` depending on the configuration.
+
+    See Also
+    --------
+    :func:`biosteam.create_conventional_wastewater_treatment_system`;
+    :func:`biosteam.create_high_rate_wastewater_treatment_system`;
+    '''
+    if WWT == 'conventional':
+        return create_conventional_wastewater_treatment_system(**WWT_kwargs)
+    elif 'high' in WWT and 'rate' in WWT:
+        return create_high_rate_wastewater_treatment_system(**WWT_kwargs)
+    else:
+        raise ValueError(f'The `WWT` value of "{WWT}" is not valid, '
+                         'please choose either "conventional" or "high-rate".')
+        
 
 from . import (
     _utils,
@@ -30,6 +81,7 @@ from . import (
 
 
 __all__ = (
+    'create_wastewater_treatment_system',
     *_utils.__all__,
     *_internal_circulation_rx.__all__,
     *_wwt_pump.__all__,
