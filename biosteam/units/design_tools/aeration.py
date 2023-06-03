4e2b855f4e69a70bd05869444da98ee6b2dfb632
Yoel <yoelcortes@gmail.com>
2023-06-02 19:50:07 -0500

    add log-mean driving force function

--- biosteam/units/design_tools/aeration.py
+++ biosteam/units/design_tools/aeration.py
@@ -5,10 +5,11 @@
 __all__ = (
     'H_O2',
     'C_O2_L',
-    'ka_L'
+    'ka_L',
+    'log_mean_driving_force',
 )
 
-from math import exp
+from math import exp, log
 
 def H_O2(T): 
     """
@@ -42,3 +43,34 @@ def ka_L(P, V, U):
     """
     return 0.002 * (P / V) ** 0.7 * U ** 0.2
 
+def log_mean_driving_force(C_sat_out, C_sat_in, C_out, C_in=None):
+    """
+    Return the driving force for mass transfer. In small vessels (<1 m tall) 
+    where both liquid concentration and saturation are almost constant, the simple
+    form is adequate [1]_. In tall vessels, the log-mean driving force should be 
+    used for more accuracy, since both the local concentration and the saturation
+    concentration are different in the top and bottom of a bioreactor [1]_. 
+    
+    Parameters
+    ----------
+    C_sat_out : float
+        Saturated concentration entering the bioreactor.
+    C_sat_out : float
+        Saturated concentration exiting the bioreactor.
+    C_out : float, optional
+        Outlet concentration. 
+    C_in : float
+        Inlet concentration. Defaults to the outlet concentration, which
+        assumes perfect mixing and oxygen uptake rate is the same everywhere.
+        
+    References
+    ----------
+    [1] Benz, G. T. Bioreactor Design for Chemical Engineers. AICHE CEP 2011, 21–26.
+
+    """
+    if C_in is None: C_in = C_out # Assume perfect mixing and oxygen uptake rate is the same everywhere.
+    dC_out = C_sat_out - C_out
+    dC_in = C_sat_in - C_in
+    if dC_out < 1e-9: # This is not theoretically possible, but may be an assumption
+        dC_out = 1e-9 # Assume it near zero
+    return (dC_out - dC_in) / log(dC_out / dC_in)
