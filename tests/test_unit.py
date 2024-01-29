5dda4d52c60fad206765416369cbb9ab02b02fcb
Yoel Cortes-Pena <yoelcortes@gmail.com>
2024-01-29 16:30:00 -0600

    fix test

--- tests/test_unit.py
+++ tests/test_unit.py
@@ -182,7 +182,8 @@ def test_process_specifications_with_recycles():
                 recycle=M1_a-0),
              P2_b,
              S1_b],
-            recycle=P1_b-0)])
+            recycle=[S3_b-1, P2_b-0, S1_b-1])]
+    )
     assert M1_b.specifications[0].path == ()
     
 def test_unit_connections():
