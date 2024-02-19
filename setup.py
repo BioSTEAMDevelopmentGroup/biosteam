2c2974d8a3188edcaff344e844c9ed38bc7f0f3a
Yoel Cortes-Pena <yoelcortes@gmail.com>
2024-02-19 17:30:44 -0600

    2.42.0 release

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     name='biosteam',
     packages=['biosteam'],
     license='MIT',
-    version='2.41.0',
+    version='2.42.0',
     description='The Biorefinery Simulation and Techno-Economic Analysis Modules',
     long_description=open('README.rst', encoding='utf-8').read(),
     author='Yoel Cortes-Pena',
