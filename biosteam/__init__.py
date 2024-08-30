32d48949e7e0266b5fb4cdf0794471bb09b75c56
Yoel <yoelcortes@gmail.com>
2024-08-30 14:02:19 -0500

    2.45.0 release

--- biosteam/__init__.py
+++ biosteam/__init__.py
@@ -13,7 +13,7 @@
 
 """
 from __future__ import annotations
-__version__ = '2.44.3'
+__version__ = '2.45.0'
 
 #: Chemical engineering plant cost index (defaults to 567.5 at 2017).
 CE: float = 567.5 
