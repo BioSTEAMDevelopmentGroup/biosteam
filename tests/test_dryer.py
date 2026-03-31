081582b4c97a4d9cd1e748ed0afc1c95f7ab0fa7
Yoel <yoelcortes@gmail.com>
2026-03-31 11:33:21 -0500

    add spaces

--- tests/test_dryer.py
+++ tests/test_dryer.py
@@ -9,7 +9,7 @@ def test_dryer_relative_humidity_and_energy_balance():
     settings.set_thermo(['Water', 'Protein', 'O2', 'N2', 'CO2', 'CH4'], cache=True)
     settings.mixture.include_excess_energies = True
 
-    feed = bst.Stream(None,Water=1000,Protein=200,units='kg/hr')
+    feed = bst.Stream(None, Water=1000, Protein=200, units='kg/hr')
     D1 = bst.DrumDryer(None,(feed,),split={"Protein":0.0})
     D1.simulate()
 
