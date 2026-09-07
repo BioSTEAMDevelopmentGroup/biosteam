b8d57d7efb267747ed0f462f58260118110400b7
Yoel <yoelcortes@gmail.com>
2026-09-07 22:27:45 +0800

    start adding test

--- tests/test_gas_fed_bioreactor.py
+++ tests/test_gas_fed_bioreactor.py
@@ -12,14 +12,16 @@ from numpy.testing import assert_allclose
 import pytest
 
 def test_gas_fed_bioreactor():
+    import biosteam as bst
     bst.settings.set_thermo(['H2', 'CO2', 'CO', 'N2', 'O2', 'H2O', 'AceticAcid'])
-
+    
     # Feeds
     media = bst.Stream(ID='media', H2O=20e3, units='kg/hr')
     H2 = bst.Stream(ID='H2', H2=100, units='kg/hr', phase='g')
     fluegas = bst.Stream(CO=23, CO2=23, H2=4.5, N2=49.5, units='m3/hr', phase='g') 
-
-    # Model acetic acid production from H2 and CO2
+    H2.F_mass = 228
+    fluegas.F_mass = 3.8e+03
+    
     # Model acetic acid production from H2 and CO2
     substrate_reaction = bst.Reaction(
         'CO + H2O -> CO2 + H2',
@@ -33,10 +35,13 @@ def test_gas_fed_bioreactor():
         ins=[media, H2, fluegas], 
         outs=('vent', 'product'), 
         tau=68, V_max=500,
-        length_to_diameter=6,
+        length_to_diameter=12,
         reactions=rxn, 
         substrate_reactions=substrate_reaction,
         gas_substrates=('H2', 'CO', 'CO2'),
+        controlled_gas_substrates=('H2', 'CO2'),
+        titer={'AceticAcid': 72},
+        controlled_feeds=[1, 2],
         design='Bubble column',
         kW_per_m3=0,
         batch=False,
